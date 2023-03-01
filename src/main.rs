05d265ee46d1599310aa20271680ebc6dfdf3793
situ2001 <yongcong2001@outlook.com>
2023-03-01 18:14:48 +0800

    feat: basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,59 @@
-use bitvec::prelude::*;
-use image::{io::Reader as ImageReader, GenericImage, GenericImageView};
-use regex::Regex;
+use clap::Parser;
+use image::io::Reader as ImageReader;
 use std::error::Error;
 
-use lsb_image::PixelGenerator;
+use image_lsb::{ImageDecoder, ImageEncoder};
 
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// mode: encode or decode
+    mode: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+    /// The path to save the file
+    save_path: Option<std::path::PathBuf>,
+    /// The seed
+    #[arg(short = 'S')]
+    seed: Option<u64>,
+    /// The payload
+    #[arg(short = 'P')]
+    payload: Option<String>,
+}
+
+#[allow(unused_must_use)]
 fn main() -> Result<(), Box<dyn Error>> {
-    let args = std::env::args().collect::<Vec<_>>();
+    let args = Cli::parse();
 
-    if args.len() == 2 {
-        //println!("usage: main <input_path> <payload_string>");
-        return Ok(());
-    }
-
-    dbg!(args);
-    // let input_path = &args[1];
-    // let payload_str = &args[2];
-
-    let mut img = ImageReader::open("./input.png")?.decode()?;
-
-    // get width and height
-    let (width, height) = img.dimensions();
-    //println!("width: {}, height: {}", width, height);
-    let mut gen = PixelGenerator::new(114514, width, height);
-
-    let payload_str = "situ2001, 114514 is a good number";
-
-    // first fill metadata
-    let metadata_str = format!("{{{{{}}}}}", payload_str.len());
-    //println!("{}", metadata_str);
-    // concat it to payload string
-    let payload_str = format!("{}{}", metadata_str, payload_str);
-
-    // fill the payload string
-    for ch in payload_str.as_bytes() {
-        //println!("Current char: {}", ch.as_char().unwrap());
-        let bits = ch.view_bits::<Msb0>();
-        for bit in bits {
-            let (x, y) = gen.next();
-            // println!("bit: {}", if bit == true { 1 } else { 0 });
-            let bit_to_be_filled = if bit == true { 1 } else { 0 };
-            let mut new_pixel = img.get_pixel(x, y);
-            new_pixel.0[0] = new_pixel.0[0] & 0b11111110 | bit_to_be_filled;
-            img.put_pixel(x, y, new_pixel);
+    if args.mode.to_lowercase() == "encode" {
+        if let None = args.save_path {
+            panic!("Save path is required");
         }
-        //println!("{} => {:?}", ch, bits);
-    }
-    // save the new image
-    img.save("./output.png")?;
-
-    {
-        let mut gen = PixelGenerator::new(114514, width, height);
-        // decode the image
-        let img = ImageReader::open("./output.png")?.decode()?;
-        let (width, height) = img.dimensions();
-        //println!("width: {}, height: {}", width, height);
-
-        let payload_string_len: usize;
-
-        // read string like {{len}} from image
-        let mut metadata_string = String::new();
-        let mut current_char_bits: Vec<u8> = Vec::new();
-        loop {
-            let (x, y) = gen.next();
-            let pixel = img.get_pixel(x, y);
-            let bit = pixel.0[0] & 0b00000001;
-            // println!("bit: {}", bit);
-            current_char_bits.push(bit);
-            if current_char_bits.len() >= 8 {
-                let collected_char = current_char_bits
-                    .iter()
-                    .fold(0, |acc, &bit| (acc << 1) | bit);
-                // convert to char and append to string
-                metadata_string.push(collected_char as char);
-                // println!("Collected: {}", collected_char as char);
-                current_char_bits.clear();
-            }
-            if metadata_string.len() == 2 && metadata_string != "{{" {
-                //println!("Invalid metadata");
-                return Ok(());
-            }
-            // regex match {{.*}}
-            if metadata_string.len() >= 4 && metadata_string.ends_with("}}") {
-                let re = Regex::new(r"\{\{(\d+)\}\}").unwrap();
-                if let Some(caps) = re.captures(&metadata_string) {
-                    payload_string_len = caps.get(1).unwrap().as_str().parse::<usize>().unwrap();
-                    //println!("Payload len: {}", payload_string_len);
-                    break;
-                }
-            }
+        if let None = args.payload {
+            panic!("Payload is required");
+        }
+        if let None = args.seed {
+            panic!("Seed is required");
         }
 
-        let mut payload = String::new();
-        let mut current_char_bits: Vec<u8> = Vec::new();
-        for _ in 0..payload_string_len * 8 {
-            let (x, y) = gen.next();
-            let pixel = img.get_pixel(x, y);
-            let bit = pixel.0[0] & 0b00000001;
-            // println!("bit: {}", bit);
-            current_char_bits.push(bit);
-            if current_char_bits.len() >= 8 {
-                let collected_char = current_char_bits
-                    .iter()
-                    .fold(0, |acc, &bit| (acc << 1) | bit);
-                // convert to char and append to string
-                payload.push(collected_char as char);
-                current_char_bits.clear();
-            }
+        if let Some(seed) = args.seed {
+            let img = ImageReader::open(args.path)?.decode()?;
+            let mut encoder = ImageEncoder::new(img, seed);
+            encoder.write_str("situ2001");
+            encoder.get_back_image().save(args.save_path.unwrap());
         }
-        println!("Decoded: {}", payload);
+    } else if args.mode.to_lowercase() == "decode" {
+        if let None = args.seed {
+            panic!("Seed is required");
+        }
+
+        if let Some(seed) = args.seed {
+            let img = ImageReader::open(args.path)?.decode()?;
+            let mut decoder = ImageDecoder::new(img, seed);
+            println!("Decoded: {}", decoder.read().unwrap());
+        }
+    } else {
+        panic!("Invalid mode");
     }
 
     Ok(())
